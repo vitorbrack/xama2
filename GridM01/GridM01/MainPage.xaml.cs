f428e867deb102dda7acacb0ee7722acf1b3e6fa
Pedro <pedro.vcosta405@gmail.com>
2021-09-20 11:54:52 -0300

    Adicionar arquivos de projeto.

---
+++ GridM01/GridM01/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GridM01
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtVoltar(object sender, EventArgs e)
+        {
+            Navigation.PopToRootAsync();
+        }
+
+        private void btIr(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Pagina03());
+        }
+    }
+}
---
+++ GridM01/GridM01/Pagina03.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GridM01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina03 : ContentPage
+    {
+        public Pagina03()
+        {
+            InitializeComponent();
+        }
+
+        private void btVoltar(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}

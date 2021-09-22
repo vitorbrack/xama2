a8d385285a25075c9812c6d267f176ee7c442d4e
vitorbrack <vitorodmiranda@gmail.com>
2021-09-22 09:03:36 -0300

    oi

--- GridM01/GridM01/Calculadora.xaml.cs
+++ GridM01/GridM01/Calculadora.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace GridM01.Droid
+namespace GridM01
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Calculadora : ContentPage
